15bce269f2f1d0d89d4957495a917734a95028d0
SeanRobinette <smrobinette@gmail.com>
2019-10-20 02:27:17 -0400

    rename main class

--- Spring 5 Beginner To Guru/_Pet Clinic App/pet-clinic-web/src/main/java/com/robinette/SpringPetClinicApplication.java
+++ Spring 5 Beginner To Guru/_Pet Clinic App/pet-clinic-web/src/main/java/com/robinette/SpringPetClinicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring5b2gPetClinicApplication {
+public class SpringPetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring5b2gPetClinicApplication.class, args);
+        SpringApplication.run(SpringPetClinicApplication.class, args);
     }
 
 }
