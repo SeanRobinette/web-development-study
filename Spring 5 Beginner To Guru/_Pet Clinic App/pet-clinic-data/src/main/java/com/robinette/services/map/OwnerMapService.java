491b7fe10e9f6488aaa31c86d0d9675237237425
SeanRobinette <smrobinette@gmail.com>
2019-10-19 23:54:40 -0400

    Update services, add home page

---
+++ Spring 5 Beginner To Guru/_Pet Clinic App/pet-clinic-data/src/main/java/com/robinette/services/map/OwnerMapService.java
@@ -0,0 +1,43 @@
+package com.robinette.services.map;
+
+import java.util.Set;
+
+import com.robinette.model.Owner;
+import com.robinette.services.CrudService;
+import com.robinette.services.OwnerService;
+
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void deleteByObject(Owner obj) {
+        super.deleteByObject(obj);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Long id, Owner object) {
+        return super.save(id, object);
+    }
+    public Owner findByLastName(String lastName) {
+        return map.entrySet()
+                  .stream()
+                  .filter(obj -> {return obj.getValue().getLastName() == lastName;})
+                  .findAny()
+                  .orElse(null)
+                  .getValue();
+    }
+}
