07e037d25e25cc238c51d4c3cdca7a05dd141a13
SeanRobinette <smrobinette@gmail.com>
2019-10-20 09:24:06 -0400

    Implement SpecialtyService

---
+++ Spring 5 Beginner To Guru/_Pet Clinic App/pet-clinic-data/src/main/java/com/robinette/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.robinette.services;
+
+import com.robinette.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ Spring 5 Beginner To Guru/_Pet Clinic App/pet-clinic-data/src/main/java/com/robinette/services/map/SpecialtyMapService.java
@@ -0,0 +1,35 @@
+package com.robinette.services.map;
+
+import com.robinette.model.Specialty;
+import com.robinette.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void deleteByObject(Specialty obj) {
+        super.deleteByObject(obj);
+    }
+}
