de79d68e70e81fb78e8672876d271da3b1e2cf35
SeanRobinette <smrobinette@gmail.com>
2019-10-21 07:35:22 -0400

    Finish updating Recipe App with Lombok

--- Spring 5 Beginner To Guru/Ch 09 Project Lombok/Recipe App/src/main/java/com/robinette/recipe/model/Ingredient.java
+++ Spring 5 Beginner To Guru/Ch 09 Project Lombok/Recipe App/src/main/java/com/robinette/recipe/model/Ingredient.java
@@ -6,7 +6,9 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
+@NoArgsConstructor
 public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
