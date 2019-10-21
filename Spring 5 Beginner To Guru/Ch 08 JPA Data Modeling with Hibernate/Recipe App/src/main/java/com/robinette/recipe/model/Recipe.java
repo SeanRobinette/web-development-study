84f94f2299e7f0fdc279ca14441b2632898c273f
SeanRobinette <smrobinette@gmail.com>
2019-10-20 23:42:30 -0400

    Fill in fields for Recipe class

--- Spring 5 Beginner To Guru/Ch 08 JPA Data Modeling with Hibernate/Recipe App/src/main/java/com/robinette/recipe/model/Recipe.java
+++ Spring 5 Beginner To Guru/Ch 08 JPA Data Modeling with Hibernate/Recipe App/src/main/java/com/robinette/recipe/model/Recipe.java
@@ -1,4 +1,14 @@
 package com.robinette.recipe.model;
 
 public class Recipe {
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    //todo add
+    //private Difficulty difficulty
+    private Byte[] image;
 }
