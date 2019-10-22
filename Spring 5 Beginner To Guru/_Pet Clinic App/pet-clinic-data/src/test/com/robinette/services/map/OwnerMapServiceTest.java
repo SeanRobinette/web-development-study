827e73f85c939356a10aea20454911c5a3a2f9cc
SeanRobinette <smrobinette@gmail.com>
2019-10-22 04:03:23 -0400

    Convert to JUnit 5, add basic tests

---
+++ Spring 5 Beginner To Guru/_Pet Clinic App/pet-clinic-data/src/test/com/robinette/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,34 @@
+package com.robinette.services.map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerMapServiceTest {
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void deleteByObject() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}
---
+++ Spring 5 Beginner To Guru/_Pet Clinic App/pet-clinic-web/src/test/com/robinette/SpringPetClinicApplicationTest.java
@@ -0,0 +1,16 @@
+package com.robinette;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class SpringPetClinicApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
