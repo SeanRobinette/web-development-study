b4680613c45f630cd91697d64444def3e55af367
SeanRobinette <smrobinette@gmail.com>
2019-10-23 00:48:44 -0400

    React Tutorial - Add "Time Travel", completing tutorial

--- React/Reactjs.org Tutorial/src/index.js
+++ React/Reactjs.org Tutorial/src/index.js
@@ -52,11 +52,12 @@ class Game extends React.Component {
                 }
             ],
             xIsNext: true,
+            stepNumber: 0
         };
     }
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
@@ -64,7 +65,7 @@ class Game extends React.Component {
                 'Go to move #' + move :
                 'Go to game start';
             return (
-                <li>
+                <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             );
@@ -92,7 +93,7 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if(calculateWinner(squares) || squares[i]) {
@@ -103,7 +104,14 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
-            xIsNext: !this.state.xIsNext
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length
+        });
+    }
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step%2) === 0,
         });
     }
 }
