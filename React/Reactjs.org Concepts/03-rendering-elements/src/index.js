17ac8ed5efd9cf1695be78b102ebc103f1bcb514
SeanRobinette <smrobinette@gmail.com>
2019-10-23 02:28:38 -0400

    React Concepts - Write a clock ticker, Closes #3

--- React/Reactjs.org Concepts/03-rendering-elements/src/index.js
+++ React/Reactjs.org Concepts/03-rendering-elements/src/index.js
@@ -4,7 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Update the view once per second
+// Note that the entire element is generated once per update, because elements are immutable
+// React handles making sure that the DOM is only updated with changes that are actually needed
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+        )
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+const ticker = setInterval(tick, 1000)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
