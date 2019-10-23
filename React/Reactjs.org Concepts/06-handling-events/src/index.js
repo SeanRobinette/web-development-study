2422e5c147a09261e0dfb2c018cca29d50fc0533
SeanRobinette <smrobinette@gmail.com>
2019-10-23 18:12:45 -0400

    React - Demonstrate event handling and `this` binding, Closes #6

--- React/Reactjs.org Concepts/06-handling-events/src/index.js
+++ React/Reactjs.org Concepts/06-handling-events/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 class ActionLink extends React.Component {
     handleClick(e) {
         e.preventDefault();
@@ -14,7 +15,51 @@ class ActionLink extends React.Component {
         );
     }
 }
-ReactDOM.render(<ActionLink />, document.getElementById('root'));
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+
+        // Bind the handler so that `this` always refers to this object and 
+        // not the handler's caller
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
+    }
+
+    render() {
+        return(
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        );
+    }
+}
+// Using an arrow function ensures `this` binding, but creates the function for every new instance
+class LoggingButton extends React.Component {
+    handleClick() {
+        console.log('this is:' + this);
+    }
+
+    render() {
+        return(
+            <button onClick={(e) => this.handleClick(e)}>Test</button>
+        )
+    }
+}
+
+const body = (
+    <div>
+        <ActionLink /><br/>
+        <Toggle /><br/>
+        <LoggingButton />
+    </div>
+)
+ReactDOM.render(body, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
