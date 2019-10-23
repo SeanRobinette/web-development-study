d7d69611d12e235e3627f624802eb49239c26d71
SeanRobinette <smrobinette@gmail.com>
2019-10-23 05:26:42 -0400

    Spring - Fix custom login page & keep CSRF token

--- Spring/Spring & Hibernate for Beginners (includes Spring Boot)/security-demo/src/main/java/com/seanrobinette/securitydemo/config/SecurityConfiguration.java
+++ Spring/Spring & Hibernate for Beginners (includes Spring Boot)/security-demo/src/main/java/com/seanrobinette/securitydemo/config/SecurityConfiguration.java
@@ -2,9 +2,12 @@ package com.seanrobinette.securitydemo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +21,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser(user.username("susan").password("abc").roles("user").build())
                 .withUser(user.username("mary").password("123").roles("manager").build())
                 .withUser(user.username("sean").password("abc").roles("admin").build());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/login*")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/showLoginPage")
+                    .defaultSuccessUrl("/")
+                    .successHandler(new SavedRequestAwareAuthenticationSuccessHandler())
+                    .loginProcessingUrl("/login")
+                    .permitAll()
+        ;
     }
 }
---
+++ Spring/Spring & Hibernate for Beginners (includes Spring Boot)/security-demo/src/main/java/com/seanrobinette/securitydemo/controllers/LoginController.java
@@ -0,0 +1,16 @@
+package com.seanrobinette.securitydemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/showLoginPage")
+    public String showLoginPage(Model model, @RequestParam(value = "error", required = false) String error) {
+        model.addAttribute("error", error != null);
+        return "login";
+    }
+}
