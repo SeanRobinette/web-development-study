b8fb1d3b0f8c19fe543d857282c7bee58d10c109
SeanRobinette <smrobinette@gmail.com>
2019-10-23 01:52:53 -0400

    React Concepts - Write Hello, World! program, Closes #1

--- React/Reactjs.org Concepts/01-hello-world/src/index.js
+++ React/Reactjs.org Concepts/01-hello-world/src/index.js
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Render a Hello, world! message
+ReactDOM.render(
+    <h1>Hello, world!</h1>,         // The message to render
+    document.getElementById('root') // Where to render it
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
