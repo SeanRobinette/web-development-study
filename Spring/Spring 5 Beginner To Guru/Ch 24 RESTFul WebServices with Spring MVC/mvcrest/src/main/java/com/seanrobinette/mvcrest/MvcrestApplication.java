b4530d73d0dfb3743ce5ed082545e3b30d46a99b
SeanRobinette <smrobinette@gmail.com>
2019-10-30 14:48:08 -0400

    Spring - Create Ch24 project

---
+++ Spring/Spring 5 Beginner To Guru/Ch 24 RESTFul WebServices with Spring MVC/mvcrest/src/main/java/com/seanrobinette/mvcrest/MvcrestApplication.java
@@ -0,0 +1,13 @@
+package com.seanrobinette.mvcrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcrestApplication.class, args);
+	}
+
+}
---
+++ Spring/Spring 5 Beginner To Guru/Ch 24 RESTFul WebServices with Spring MVC/mvcrest/src/test/java/com/seanrobinette/mvcrest/MvcrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanrobinette.mvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
